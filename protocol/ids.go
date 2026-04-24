c5ded4c37d22fed178b45bd286a8622fc20aeb0b
Elias Mawa <elias@mawa.dev>
2026-04-24 11:54:54 -0600

    feat: expand idAdverbs and idAdjectives for more diverse tunnel ID generation

--- protocol/ids.go
+++ protocol/ids.go
@@ -6,19 +6,65 @@ import (
 	"strings"
 )
 
+var idAdverbs = []string{
+	"arcaneily", "blindly", "boldly", "brightly", "calmly", "chaotically", "clearly",
+	"coldly", "covertly", "cruelly", "cryptically", "darkly", "dauntlessly", "deeply",
+	"deftly", "dimly", "distantly", "divinely", "dreadfully", "dryly", "eerily",
+	"eldritchly", "endlessly", "eternally", "evilly", "faintly", "fearlessly", "fiercely",
+	"firmly", "forebodingly", "freely", "frostily", "fully", "ghostly", "ghoulishly",
+	"gravelely", "grimly", "hauntingly", "harshly", "heavily", "hellishly", "hollowly",
+	"icily", "infernally", "keenly", "lethally", "lightly", "liminally", "lowly",
+	"magically", "malevolently", "menacingly", "mercilessly", "mutely", "mystically",
+	"nimbly", "nobly", "obscurely", "ominously", "openly", "perilously", "phantomly",
+	"proudly", "quietly", "rapidly", "rarely", "relentlessly", "roughly", "ruinously",
+	"savagely", "sharply", "silently", "sinisterly", "slowly", "softly", "solemnly",
+	"solidly", "spectrally", "starkly", "stealthily", "sternly", "stolidly", "strongly",
+	"subtly", "swiftly", "terribly", "thinly", "treacherously", "truly", "undyingly",
+	"unholy", "vastly", "vengefully", "vividly", "voraciously", "wickedly", "wildly",
+	"wisely", "wrathfully", "wryly",
+}
+
 var idAdjectives = []string{
-	"ancient", "arcane", "astral", "bold", "brave", "chaotic", "cryptic", "dark", "elder",
-	"ethereal", "fierce", "frozen", "hidden", "icy", "jade", "keen", "liquid", "mighty",
-	"mystic", "noble", "obscure", "potent", "quick", "radiant", "silent", "swift",
-	"twilight", "uncanny", "vivid", "wandering", "wild",
+	"abyssal", "accursed", "ancient", "arcane", "ashen", "astral", "banished", "battered",
+	"bewitched", "bleak", "blighted", "bloodied", "bold", "bonded", "brave", "broken",
+	"burning", "celestial", "chaotic", "charmed", "chromatic", "cold", "corrupted",
+	"crimson", "cryptic", "cursed", "dark", "dead", "deathly", "defiled", "demonic",
+	"destined", "diabolical", "distant", "divine", "doomed", "draconic", "dread",
+	"druidic", "dry", "dwarven", "dying", "elder", "eldritch", "elven", "empty",
+	"enchanted", "ethereal", "exalted", "fallen", "feral", "fierce", "fiendish",
+	"flaming", "forbidden", "forgotten", "forsaken", "foul", "frozen", "furtive",
+	"ghostly", "gilded", "glowing", "grim", "hallowed", "haunted", "hellish", "heretical",
+	"hidden", "hollow", "holy", "hungry", "icy", "infernal", "iron", "jade", "keen",
+	"legendary", "lethal", "liquid", "lost", "luminous", "lurking", "mad", "malevolent",
+	"mighty", "molten", "moonlit", "mournful", "murky", "mystic", "necrotic", "noble",
+	"obscure", "ominous", "pale", "petrified", "phantom", "plagued", "potent", "primal",
+	"profane", "quick", "radiant", "raging", "ruined", "runic", "sacred", "savage",
+	"scarlet", "scorched", "sepulchral", "shadow", "shattered", "silent", "silver",
+	"sinister", "skeletal", "smoldering", "spectral", "stark", "still", "stone",
+	"storming", "sunken", "swift", "tainted", "terrible", "twilight", "twisted",
+	"umbral", "uncanny", "unholy", "unseen", "veiled", "vengeful", "vivid", "volatile",
+	"wandering", "wicked", "wild", "withered", "wrathful", "wretched",
 }
 
 var idNouns = []string{
-	"amulet", "basilisk", "cipher", "dragon", "ember", "fortress", "gargoyle", "helm",
-	"illusion", "kraken", "lich", "mithril", "nymph", "oracle", "phoenix", "rune",
-	"specter", "talisman", "void", "wyrm", "zephyr", "amethyst", "bramble", "crypt",
-	"druid", "forge", "grimoire", "haven", "isle", "lava", "moon", "nexus", "obsidian",
-	"prism", "quill", "tome", "vestige", "warden", "xorn",
+	"altar", "amulet", "anvil", "arch", "archmage", "artefact", "assassin", "axe",
+	"banshee", "basilisk", "beacon", "behemoth", "blade", "blight", "bones", "bramble",
+	"catacomb", "centaur", "chains", "chimera", "cipher", "citadel", "crypt", "curse",
+	"cyclops", "dagger", "demon", "depths", "dirge", "dragon", "druid", "dungeon",
+	"effigy", "ember", "enchantment", "exile", "familiar", "fiend", "forge", "fortress",
+	"gargoyle", "gate", "ghost", "ghoul", "giant", "goblin", "golem", "grave",
+	"grimoire", "guardian", "harbinger", "haven", "helm", "heretic", "hex", "hydra",
+	"idol", "illusion", "inferno", "isle", "jailer", "kraken", "labyrinth", "lair",
+	"lance", "leviathan", "lich", "longbow", "manticore", "mausoleum", "maze", "minotaur",
+	"mithril", "monolith", "moon", "necromancer", "nexus", "nightmare", "nymph",
+	"obsidian", "ogre", "oracle", "orc", "overlord", "paladin", "phantom", "phoenix",
+	"plague", "portal", "prism", "prophet", "quill", "ravine", "reaper", "relic",
+	"revenant", "rune", "sanctum", "sarcophagus", "scroll", "sentinel", "serpent",
+	"shade", "shard", "shrine", "siege", "skeleton", "skull", "specter", "spell",
+	"spire", "staff", "stalker", "stronghold", "sword", "talisman", "throne", "tomb",
+	"tome", "tower", "troll", "unicorn", "urn", "vampire", "vault", "vestige",
+	"void", "vortex", "warden", "warlock", "wasteland", "witch", "wizard", "wraith",
+	"wyvern", "xorn", "zealot", "zephyr", "zombie",
 }
 
 func randIndex(n int) int {
@@ -30,9 +76,10 @@ func randIndex(n int) int {
 	return int(i.Int64())
 }
 
-// RandomTunnelID returns a random three-word tunnel ID, e.g. "mighty-arcane-dragon".
+// RandomTunnelID returns a random four-word tunnel ID, e.g. "swiftly-ancient-silent-dragon".
 func RandomTunnelID() string {
 	parts := []string{
+		idAdverbs[randIndex(len(idAdverbs))],
 		idAdjectives[randIndex(len(idAdjectives))],
 		idAdjectives[randIndex(len(idAdjectives))],
 		idNouns[randIndex(len(idNouns))],
