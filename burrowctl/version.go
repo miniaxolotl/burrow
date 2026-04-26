df4d1843c6b165042bceef4f1a5865aca49cddb6
Elias Mawa <elias@mawa.dev>
2026-04-25 21:33:52 -0600

    ci: add release-please for automatic version bumping and releases

--- burrowctl/version.go
+++ burrowctl/version.go
@@ -2,4 +2,4 @@ package main
 
 // Version is the current release. release-please keeps this file in sync;
 // goreleaser overrides it at link time via -X main.Version.
-var Version = "0.1.0"
+var Version = "0.2.2"
